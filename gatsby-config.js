6479d95c66bf0bd908a8b935e143e5ca798828c4
Paul <paul@theapplegates.com>
2020-05-29 16:08:38 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-prismic',
       options: {
-        repositoryName: 'prismic-applegate-9254.netlify.app',
+        repositoryName: 'prismic',
         accessToken: `${process.env.API_KEY}`,
         // Get the correct URLs in blog posts
         linkResolver: () => (post) => `/${post.uid}`,
