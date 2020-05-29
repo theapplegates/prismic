1d528348f8dc4773571d3cded02dcde1e859d656
Paul Applegate <paul@theapplegates.com>
2020-05-29 15:48:57 -0400

    changes

--- config/website.js
+++ config/website.js
@@ -1,10 +1,10 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Starter - Prismic.io', // Navigation and Site Title
-  titleAlt: 'Gatsby Prismic.io', // Title for JSONLD
-  description: 'A typography-heavy & light-themed Gatsby Starter which uses the Headless CMS Prismic.',
-  headline: 'Writing and publishing content for LekoArts', // Headline for schema.org JSONLD
-  url: 'https://prismic.lekoarts.de', // Domain of your site. No trailing slash!
+  title: 'Living Life', // Navigation and Site Title
+  titleAlt: 'Life', // Title for JSONLD
+  description: 'Living life and writing some things down alone the way.',
+  headline: 'I blog' // Headline for schema.org JSONLD
+  url: 'https://distracted-hypatia-48b9a0.netlify.app', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo: '/logos/logo-1024.png', // Used for SEO
   ogLanguage: 'en_US', // Facebook Language
@@ -12,13 +12,13 @@ module.exports = {
   // JSONLD / Manifest
   favicon: 'src/favicon.png', // Used for manifest favicon generation
   shortName: 'Prismic', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  author: 'Applegate', // Author for schemaORGJSONLD
   themeColor: '#3D63AE',
   backgroundColor: '#EBEDF2',
 
-  twitter: '@starter_prismicio', // Twitter Username
-  facebook: 'gatsby-prismic', // Facebook Site Name
-  googleAnalyticsID: 'UA-47519312-7',
+  twitter: '@papplegate', // Twitter Username
+  facebook: 'paul.applegate', // Facebook Site Name
+  googleAnalyticsID: 'UA-147090349-8',
 
   skipNavId: 'reach-skip-nav', // ID for the "Skip to content" a11y feature
 }
